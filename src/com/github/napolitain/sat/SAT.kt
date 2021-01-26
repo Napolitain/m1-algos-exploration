c8742621ee31ab79089aeb3117c004b18ca8db2e
Maxime Boucher <mxboucher@gmail.com>
2021-01-26 10:32:55 +0100

    linkedlist > arraylist

--- src/com/github/napolitain/sat/SAT.kt
+++ src/com/github/napolitain/sat/SAT.kt
@@ -9,28 +9,30 @@ class SAT(path: String) {
 
 	// (a ou b) et (a ou c)
 	private val atoms: Set<Atom>
-	private val cnf: LinkedList<LinkedList<Atom>>
+	private val cnf: MutableList<MutableList<Atom>>
 
 	init {
 		val fileName: String = File(".").canonicalPath + "/src/" + path
 		val bufferedReader = BufferedReader(FileReader(fileName))
 		val iterator = bufferedReader.lineSequence().iterator()
 		atoms = mutableSetOf()
-		cnf = LinkedList<LinkedList<Atom>>()
+		cnf = mutableListOf()
+		var i: Int = 0;
 		while (iterator.hasNext()) {
 			val electrons: String = iterator.next()
-			cnf.add(LinkedList())
+			cnf.add(mutableListOf())
 			for (electron in electrons.split(" ")) {
 				val atom: Atom
 				if (electron.startsWith("!")) {
 					atom = Atom(electron[1].toInt())
-					cnf.last.add(!atom)
+					cnf[i].add(!atom)
 				} else {
 					atom = Atom(electron[0].toInt())
-					cnf.last.add(atom)
+					cnf[i].add(atom)
 				}
 				atoms.add(atom)
 			}
+			i++;
 		}
 		bufferedReader.close();
 	}
